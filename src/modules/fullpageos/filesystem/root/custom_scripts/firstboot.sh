f342441fc2bd5d893790d727b135b3e2e207b0e6
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2025-07-30 13:33:19 -0400

    try runningj eda script in firstboot.sh

--- src/modules/fullpageos/filesystem/root/custom_scripts/firstboot.sh
+++ src/modules/fullpageos/filesystem/root/custom_scripts/firstboot.sh
@@ -2,7 +2,17 @@
 
 set -eux
 
-# curl -s https://apt.edatec.cn/bsp/ed-install.sh | sudo bash -s hmi3010_101c
+# Check if firstboot has already run
+FIRSTBOOT_MARKER="/var/lib/firstboot_completed"
+if [ -f "$FIRSTBOOT_MARKER" ]; then
+    echo "First boot setup already completed, exiting..."
+    exit 0
+fi
 
 runuser -l pi -c 'systemctl --user enable display-rotate.service'
 runuser -l pi -c 'systemctl --user start display-rotate.service'
+
+# Mark firstboot as completed before potentially rebooting
+touch "$FIRSTBOOT_MARKER"
+
+curl -s https://apt.edatec.cn/bsp/ed-install.sh | sudo bash -s hmi3010_101c
