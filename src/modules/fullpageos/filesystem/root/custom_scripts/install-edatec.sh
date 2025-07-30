80dd8244c0edb1ecef8ea24db9a9914a97973ecf
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2025-07-30 17:16:57 -0400

    add wait for clock time sync

--- src/modules/fullpageos/filesystem/root/custom_scripts/install-edatec.sh
+++ src/modules/fullpageos/filesystem/root/custom_scripts/install-edatec.sh
@@ -8,6 +8,12 @@ if [ -f "$MARKER" ]; then
   exit 0
 fi
 
+while [ "$(date +%s)" -lt 1704067200 ]; do
+  echo "Waiting for system clock to be synced..."
+  echo $(date)
+  sleep 2
+done
+
 # Run the installer
 curl -fsSL https://apt.edatec.cn/bsp/ed-install.sh | sudo bash -s hmi3010_101c
 
