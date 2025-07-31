6b5e9d796904f7ee5829e618fb921b270a3e31d0
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2025-07-31 01:45:23 -0400

    run ip configurator in user service

--- src/modules/fullpageos/filesystem/root/custom_scripts/firstboot.sh
+++ src/modules/fullpageos/filesystem/root/custom_scripts/firstboot.sh
@@ -4,3 +4,6 @@ set -eux
 
 runuser -l pi -c 'systemctl --user enable install-edatec.service'
 runuser -l pi -c 'systemctl --user start install-edatec.service'
+
+runuser -l pi -c 'systemctl --user enable ip-configurator.service'
+runuser -l pi -c 'systemctl --user start ip-configurator.service'
