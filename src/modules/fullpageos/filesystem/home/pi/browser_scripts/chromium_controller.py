5c725992eafaae2af2311ba63c3e44c01cfe57e5
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2025-07-31 02:00:25 -0400

    make initial ip config file

--- src/modules/fullpageos/filesystem/home/pi/browser_scripts/chromium_controller.py
+++ src/modules/fullpageos/filesystem/home/pi/browser_scripts/chromium_controller.py
@@ -29,32 +29,12 @@ class ChromiumController():
         self.ip_config_file = "/home/pi/apps/ip_configurator/ip_config.json"
         self.current_jace_url = "http://localhost:8000"  # Default fallback
 
-        self.kiosk_urls = []
-        self.kiosk_urls_display_time = []
-        self.kiosk_urls_keypresses = []
-        self.spamkeys = []
-
-        with open(os.path.join(self.base_dir, "urls.txt"), "r") as kiosk_urls_file:
-            line = kiosk_urls_file.readline()
-            while line:
-                data = line.split(' ')
-                self.kiosk_urls.append(data[0])
-                self.kiosk_urls_display_time.append(int(data[-1]) if len(data) > 1 else -1)
-                self.kiosk_urls_keypresses.append(data[1:-1] if len(data) > 2 else [])
-                line = kiosk_urls_file.readline()
-
-        with open(os.path.join(self.base_dir, "spamkey.txt"), "r") as spamkey_file:
-            line = spamkey_file.readline()
-            if line:
-                self.spamkeys = line.split(' ')
 
         try:
             self.mute_time = int(sys.argv[-1])
         except ValueError:
             self.mute_time = 0
 
-        self.current_kiosk_url_index = -1
-        self.next_url_time_left = -1
         self.mute_time_left = -1
 
         self.browser = pychrome.Browser(url="http://127.0.0.1:9222")
@@ -154,12 +134,6 @@ class ChromiumController():
 
         if self.initial_load:
             self.initial_load = False
-        else:
-            for key in self.spamkeys:
-                command, data = key.split(':', 1)
-                chromium_window_id = subprocess.check_output(['xdotool', 'search', '--onlyvisible', '--class', 'chromium'], env=self.env).splitlines()[0]
-                subprocess.run(['xdotool', 'windowactivate', '--sync', chromium_window_id], check=True, env=self.env)
-                subprocess.run(['xdotool', command, data], check=True, env=self.env)
 
     def _loading_failed(self, **kwargs):
         # We only care about the main page loading, not of any subelement
